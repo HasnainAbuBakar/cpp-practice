847d08c8f8fe40dc3ad0c53bcb0f31b2e37e098f
Hasnain AbuBakar <hasnainabubakar404@gmail.com>
2024-11-15 14:41:22 +0500

    Add files via upload

---
+++ &&.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int value_One = 10;
+	int value_Two = 20;
+	int value_Three = 30;
+	
+	if(value_One<=value_Two && value_One<=value_Three)
+	{
+		cout<<"This is running"<<endl;
+	}
+	cout<<"Thank you!";
+}
