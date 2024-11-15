30778f672e8e14e32ae7f7bdc5d5ec2157127797
Hasnain AbuBakar <hasnainabubakar404@gmail.com>
2024-11-15 13:04:02 +0500

    Add files via upload

---
+++ If statement.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueOne=10;
+	int valueTwo=10;
+	
+	cout <<"Hello"<<endl;
+	cout<<"Kashmir"<<endl;
+	
+	if (valueOne == valueTwo)
+	{
+		cout<<"Value are equal "<<endl;
+		cout<<"if is runing"<<endl;
+		
+	}
+	
+	cout<<"This is not in if"<<endl;
+	cout<<"This is last line";
+}
